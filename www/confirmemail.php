962af4a40a94daac25094c43a0ef2328ab31a9c3
Charlie Meyer <chmeyer@bbn.com>
2016-01-04 14:55:47 -0500

    Add email confirmation links for new requests to auto confirm emails.
    Handlerequest.php sends link to users, their email gets confirmed
    when they click it and get sent to confirmemail.php.

---
+++ www/confirmemail.php
@@ -0,0 +1,137 @@
+<?php
+//----------------------------------------------------------------------
+// Copyright (c) 2016 Raytheon BBN Technologies
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and/or hardware specification (the "Work") to
+// deal in the Work without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Work, and to permit persons to whom the Work
+// is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Work.
+//
+// THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
+// IN THE WORK.
+//----------------------------------------------------------------------
+
+require_once('db_utils.php');
+include_once('/etc/geni-ar/settings.php');
+require_once('ar_constants.php');
+
+// Returns user's email if valid, "" otherwise.
+function confirm_email() {
+    if (array_key_exists('n', $_REQUEST) && array_key_exists('id', $_REQUEST)) {
+        $nonce = $_REQUEST['n'];
+        $db_id = $_REQUEST['id'];
+
+        $db_conn = db_conn();
+
+        $sql = "SELECT * from idp_email_confirm "
+        . "where id = " . $db_conn->quote($db_id, 'text')
+        . " and nonce = " . $db_conn->quote($nonce, 'text');
+
+        $db_result = db_fetch_row($sql, "get idp_email_confirm");
+
+        if ($db_result[RESPONSE_ARGUMENT::CODE] == RESPONSE_ERROR::NONE) {
+            $result = $db_result[RESPONSE_ARGUMENT::VALUE];
+            $email = $result['email'];
+            // actually do the email confirming 
+            // todo: ask tom stuff about what states are, about whitelist and blacklist
+            $sql = "UPDATE idp_account_request SET request_state='CONFIRM_REQUESTER'"
+                 . " where email = " . $db_conn->quote($email, 'text')
+                 . " and (request_state='REQUESTED')";
+            $update_result = db_execute_statement($sql);
+            if ($update_result[RESPONSE_ARGUMENT::CODE] != RESPONSE_ERROR::NONE) {
+                return "";
+            }
+            delete_reset($db_id, $nonce);
+            return $email;
+        } else {
+            error_log("Error getting email confirm record: " . $db_result[RESPONSE_ARGUMENT::OUTPUT]);
+            return "";
+        }
+    } else {
+        error_log("Failed to confirm email because bad url");
+        return ""; 
+    }
+}
+
+// once email is confirmed, delete the entry from idp_email_confirm
+// not clear what to do if there's a DB error here
+function delete_reset($id, $nonce) {
+    $db_conn = db_conn();
+    $sql = "delete from idp_email_confirm"
+         . " where id = " . $db_conn->quote($id, 'text')
+         . " and nonce = " . $db_conn->quote($nonce, 'text');
+    $result = db_execute_statement($sql);
+    if ($result[RESPONSE_ARGUMENT::CODE] != RESPONSE_ERROR::NONE) {
+        error_log("Error deleting old records: "
+                  . $result[RESPONSE_ARGUMENT::OUTPUT]);
+    }
+    return $result == RESPONSE_ERROR::NONE;
+}
+
+function send_success_email($email) {
+    global $AR_EMAIL_HEADERS, $idp_approval_email;
+    $subject = "GENI Email Confirmation";
+    $body = "The email you used to sign up for GENI has been successfully confirmed. \n"
+          . "You should hear from us in a few days regarding the status of your new account"
+          . "Thank you,\n"
+          . "GENI Operations\n";
+    $headers = $AR_EMAIL_HEADERS;
+    $headers .= "Cc: $idp_approval_email";
+    mail($email, $subject, $body, $headers);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>GENI: Reset Password</title>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet" type="text/css">
+<link type="text/css" href="kmtool.css" rel="Stylesheet"/>
+</head>
+<body>
+<div id="content">
+<a id='geni_logo' href="http://www.geni.net" target="_blank">
+<img src="geni.png" width="88" height="75" alt="GENI"/>
+</a>
+
+<?php 
+$email = confirm_email();
+
+if($email != "") { 
+    print "<h2>Email address $email successfully confrimed</h2>";
+    print "<p>You should hear from us in a few days regarding the status of your new account</p>";
+} else {
+    print "<h2>Invalid request to password change page</h2>";
+}
+
+?>
+
+</div>
+
+<div id="footer">
+Need help? Questions? Email 
+<a href="mailto:help@geni.net">GENI Help
+help@geni.net</a>.
+<br>
+<a href="http://www.geni.net/">GENI</a>
+is sponsored by the
+<a href="http://www.nsf.gov/">
+  <img src="https://www.nsf.gov/images/logos/nsf1.gif"
+       alt="NSF Logo" height="25" width="25">
+  National Science Foundation
+</a>
+</div>
+</body>
+</html>
--- www/handlerequest.php
+++ www/handlerequest.php
@@ -29,6 +29,45 @@ require_once('ssha.php');
 require_once('ar_constants.php');
 include_once('/etc/geni-ar/settings.php');
 
+// Generate a random string (nums, uppercase, lowercase) of width $width
+function random_id($width=6) {
+    $result = '';
+    for ($i=0; $i < $width; $i++) { 
+        $result .= base_convert(strval(rand(0, 35)), 10, 36);
+    }
+    return strtoupper($result);
+}
+
+// make a new /confirmemail.php?id=XXX&n=YYY link for use in email confirmation email
+function create_email_confirm_link($base_path, $id1, $id2) {
+    global $acct_manager_url;
+    $base_url = parse_url($acct_manager_url);
+    $path = dirname($base_path);
+    $path .= "/confirmemail.php?id=$id1&n=$id2";
+    $url = $base_url["scheme"] . "://" . $base_url["host"] . "$path";
+    return $url;
+}
+
+// Insert the password change request into the idp_email_confirm table
+function insert_email_confirm($email, $nonce) {
+    $db_conn = db_conn();
+    $sql = "insert into idp_email_confirm (email, nonce) values (";
+    $sql .= $db_conn->quote($email, 'text');
+    $sql .= ', ';
+    $sql .= $db_conn->quote($nonce, 'text');
+    $sql .= ") returning id, created";
+
+    $db_result = db_fetch_row($sql, "insert idp_email_confirm");
+    $result = false;
+    if ($db_result[RESPONSE_ARGUMENT::CODE] == RESPONSE_ERROR::NONE) {
+        $result = $db_result[RESPONSE_ARGUMENT::VALUE];
+    } else {
+        error_log("Error inserting password reset record: "
+                  . $db_result[RESPONSE_ARGUMENT::OUTPUT]);
+    }
+    return $result;
+}
+
 // Email admins about new request
 function send_admin_confirmation_email() {
   global $AR_EMAIL_HEADERS, $idp_approval_email, $acct_manager_url;
@@ -36,8 +75,7 @@ function send_admin_confirmation_email() {
   $subject = "New GENI Identity Provider Account Request on $server_host";
   $body = 'A new IdP account request has been submitted on host ';
   $body .= "$server_host.\n\n";
-  $email_vars = array('first_name', 'last_name', 'email',
-                    'organization', 'title', 'reason');
+  $email_vars = array('first_name', 'last_name', 'email', 'organization', 'title', 'reason');
   foreach ($email_vars as $var) {
     $val = $_REQUEST[$var];
     $body .= "$var: $val\n";
@@ -49,10 +87,11 @@ function send_admin_confirmation_email() {
 }
 
 // Email requester about their request
-function send_user_confirmation_email($user_email) {
+function send_user_confirmation_email($user_email, $confirm_url) {
   global $AR_EMAIL_HEADERS;
   $subject = "GENI Identity Provider Account Request Received";
-  $body = 'Thank you for requesting an Identity Provider account with GENI.  ';
+  $body = 'Thank you for requesting an Identity Provider account with GENI.';
+  $body .= "Please confirm your email address by clicking this link:\n $confirm_url \n";
   $body .= "You will be contacted if there are any questions about your request and notified when the account has been created.";
   $headers = $AR_EMAIL_HEADERS;
   mail($user_email, $subject, $body, $headers);
@@ -276,10 +315,20 @@ if ($errors) {
     print "<p> We are sorry, your account request failed.";
     print "An email has been sent to the operators and they will be in touch with you shortly.</p>";
   } else {
-    send_admin_confirmation_email();
-    send_user_confirmation_email($email);
-    print "<h2>Account request received.</h2>";
-    print "<p>Congratulations, your account request has been received. We will be in touch with you about the status of your request. </p>";
+    $nonce = random_id(8);
+    $db_result = insert_email_confirm($email, $nonce);
+    if ($db_result) {
+      $db_id = $db_result['id'];
+      $confirm_url = create_email_confirm_link($_SERVER['PHP_SELF'], $db_id, $nonce);
+      send_user_confirmation_email($email, $confirm_url);
+      send_admin_confirmation_email();
+      print "<h2>Account request received.</h2>";
+      print "<p>Your account request has been received. ";
+      print "Please check your email and click the confirmation link sent from {OUR-EMAIL-HERE} </p>";
+    } else {
+      // todo: what to do in this case. same as above when DB fails?
+      print "<h1>ERROR</h1>";
+    }
   }
 }
 
